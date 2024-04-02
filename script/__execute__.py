575ce4d76a190be228b04ddabdf8915e89cda035
hodhodhod <35979840+hodhodhod@users.noreply.github.com>
2024-04-02 15:04:03 +0900

    change logger object

--- script/__execute__.py
+++ script/__execute__.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     sys.argv.pop(0)
     __module_name = f"script.{sys.argv[0]}".replace("-", "_")
     __executable = importlib.import_module(__module_name)
-    __executable.execute()
+    __executable.execute(*sys.argv[1:])
