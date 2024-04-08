410e1ce6b600cb65385ad8cebbbd43f3510f1233
hodhodhod <35979840+hodhodhod@users.noreply.github.com>
2024-04-08 12:37:50 +0900

    add cspell to python dictionary

--- script/__execute__.py
+++ script/__execute__.py
@@ -3,6 +3,7 @@ import sys
 
 if __name__ == "__main__":
     sys.argv.pop(0)
+    # NOTE: Convert kebab case to snake case
     __module_name = f"script.{sys.argv[0]}".replace("-", "_")
     __executable = importlib.import_module(__module_name)
     __executable.execute(*sys.argv[1:])
