b92f0c57820c4c4d91614e8fd156efe61511c5c3
hodhodhod <35979840+hodhodhod@users.noreply.github.com>
2024-12-11 20:12:00 +0900

    fix: logger

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -16,7 +16,7 @@ LOG_DIR = Path(ENV.get("LOG_DIR", "./logs"))
 TIMEZONE = ZoneInfo(ENV.get("TZ", "UTC"))
 
 
-def configure_root_logger() -> None:
+def configure_logger(logger: logging.Logger) -> None:
     formatter = logging.Formatter(
         fmt="[%(asctime)s]:%(levelname)s:%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S(%Z)",
@@ -26,25 +26,32 @@ def configure_root_logger() -> None:
     log_file.parent.mkdir(parents=True, exist_ok=True)
 
     stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(LOG_LEVEL)
+    stream_handler.setFormatter(formatter)
     file_handler = logging.FileHandler(log_file)
-    handlers = [stream_handler, file_handler]
-    for handler in handlers:
-        handler.setLevel(LOG_LEVEL)
-        handler.setFormatter(formatter)
-    logging.basicConfig(level=LOG_LEVEL, handlers=handlers)
+    file_handler.setLevel(LOG_LEVEL)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
     logging.info("log file is %s", log_file)
     logging.info("log level is %s", LOG_LEVEL)
     logging.info("timezone is %s", TIMEZONE)
+
+
+def configure_root_logger() -> None:
+    root = logging.getLogger()
+    configure_logger(root)
 
 
 class Logger(logging.getLoggerClass()):
     _is_instanced = False
 
     def __new__(cls) -> logging.Logger:
+        logger = logging.getLogger(__name__)
         if not cls._is_instanced:
             cls._is_instanced = True
-            # configure_root_logger()
-        return logging.getLogger(__name__)
+            configure_logger(logger)
+        return logger
 
 
 LOGGER = Logger()
